1c427c7167a3bedbffa49a7c4d82540a8e5a2e47
Kristoffer Brabrand <kristoffer@brabrand.no>
2016-08-12 02:12:51 +0200

    Add base64 image to image while waiting

--- index.js
+++ index.js
@@ -4,14 +4,26 @@ var SuperPreviewClient = require('super-preview-client');
 var client = new SuperPreviewClient();
 
 module.exports = React.createClass({
+    getInitialState: function() {
+        return {
+            loading: true
+        }
+    }
+
     propTypes: {
         src: React.PropTypes.string.isRequired,
         compressed: React.PropTypes.string
     },
 
     render: function render() {
-        var div = React.createFactory('div');
+        var imageData = client.assemble(compressed);
 
-        return div({ className: 'my-div' });
+        var div = React.createFactory('img', {
+            onLoad: this.handleLoadEvent,
+            className={ this.state.loading ? "loading" : '' }
+            style: 'background-image:url(data:image/jpeg;base64,' + imageData.base64 + ')'
+        });
+
+        return div;
     }
 })
